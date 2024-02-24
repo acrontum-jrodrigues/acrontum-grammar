1ee8c8c571b8e2999a4f8e99f8d2f7d6558841dd
Joao Rodrigues <joao.rodrigues@hey.com>
2024-02-24 16:52:09 +0000

    Update styling

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from 'vue-router';
+import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Acrontum logo" class="logo" src="@/assets/acrontum_logo.png" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Welcome" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
