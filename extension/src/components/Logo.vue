0ab99a7c5bcc7eee22f0b14c7191ed424a8fa4ec
Joao Rodrigues <joao.rodrigues@hey.com>
2024-02-26 17:32:56 +0000

    Add Acrontum logo and update GitHub link

--- extension/src/components/Logo.vue
+++ extension/src/components/Logo.vue
@@ -1,5 +1,16 @@
 <template>
-  <a class="icon-btn mx-2 text-2xl" rel="noreferrer" href="https://github.com/antfu/vitesse-webext" target="_blank" title="GitHub">
-    <pixelarticons-power />
+  <a
+    class="icon-btn mx-2 text-2xl"
+    rel="noreferrer"
+    href="https://github.com/acrontum-jrodrigues/acrontum-grammar"
+    target="_blank"
+    title="GitHub"
+  >
+    <img
+      alt="Acrontum"
+      src="/assets/acrontum_logo.png"
+      width="32"
+      height="32"
+    />
   </a>
 </template>
--- extension/src/contentScripts/views/App.vue
+++ extension/src/contentScripts/views/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
-import { useToggle } from '@vueuse/core'
-import 'uno.css'
+import { useToggle } from "@vueuse/core";
+import "uno.css";
 
-const [show, toggle] = useToggle(false)
+const [show, toggle] = useToggle(false);
 </script>
 
 <template>
-  <div class="fixed right-0 bottom-0 m-5 z-100 flex items-end font-sans select-none leading-1em">
+  <div
+    class="fixed right-0 bottom-0 m-5 z-100 flex items-end font-sans select-none leading-1em"
+  >
     <div
       class="bg-white text-gray-800 rounded-lg shadow w-max h-min"
       p="x-4 y-2"
@@ -14,14 +16,12 @@ const [show, toggle] = useToggle(false)
       transition="opacity duration-300"
       :class="show ? 'opacity-100' : 'opacity-0'"
     >
-      <h1 class="text-lg">
-        Vitesse WebExt
-      </h1>
+      <h1 class="text-lg">Acrontum Grammar</h1>
       <SharedSubtitle />
     </div>
     <button
       class="flex w-10 h-10 rounded-full shadow cursor-pointer border-none"
-      bg="teal-600 hover:teal-700"
+      bg="blue-400 hover:blue-500"
       @click="toggle()"
     >
       <pixelarticons-power class="block m-auto text-white text-lg" />
