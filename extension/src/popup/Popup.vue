a4a9c4a152292fb297e61b4db6f01f0cda737e35
Joao Rodrigues <joao.rodrigues@hey.com>
2024-02-27 15:59:29 +0000

    Remove storageDemo from Popup.vue

--- extension/src/popup/Popup.vue
+++ extension/src/popup/Popup.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { webAppUrl } from '~/env';
-import { storageDemo } from '~/logic/storage';
 
 function openWebApp() {
   browser.tabs.create({ url: webAppUrl });
@@ -13,8 +12,5 @@ function openWebApp() {
     <SharedSubtitle />
 
     <button class="btn mt-2" @click="openWebApp">Open Web App</button>
-    <div class="mt-2">
-      <span class="opacity-50">Storage:</span> {{ storageDemo }}
-    </div>
   </main>
 </template>
